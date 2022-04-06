60a80c34b8d05d8f20119c13649066188ed5f15a
ManhKhoa1507 <nguyenlemanhkhoa15072001@gmail.com>
2022-04-06 19:43:19 +0700

    Add payload

--- test_pe.py
+++ test_pe.py
@@ -1,15 +1,11 @@
+from importlib.metadata import entry_points
 import pefile
+import pwn
+
+# Calc entry point virtual address = entry_point + image_base
 
 
-def get_virtual_address(pe):
-    for section in pe.sections:
-        print(section.Name, hex(section.VirtualAddress),
-              hex(section.Misc_VirtualSize), hex(section.SizeOfRawData))
-        
-try:
-    path = 'C:\\Users\\ADmin\\Desktop\\TestPlace\\NOTEPAD.exe'
-    pe = pefile.PE(path)
-
+def calc_entry_point_va(pe):
     # Get entry_point and image_base of pe file
     entry_point = pe.OPTIONAL_HEADER.AddressOfEntryPoint
     image_base = pe.OPTIONAL_HEADER.ImageBase
@@ -17,13 +13,56 @@ try:
     # Calc the entry_point_virtual_address = entry_point + image_based
     entry_point_va = entry_point + image_base
 
-    # print(pe.get_memory_mapped_image()[entry_point:entry_point+100])
+    return entry_point_va
+
+# Get the rsrc section
+
+
+def get_rsrc_section(pe):
+    rsrc_section = []
+    for section in pe.sections:
+        rsrc_section.append([section.Name, hex(section.Misc_VirtualSize), hex(section.VirtualAddress),
+                             hex(section.SizeOfRawData), hex(section.PointerToRawData)])
+
+    return rsrc_section[2][2], rsrc_section[2][4]
+
+# Offset = RA - Section RA = VA - Section VA
+
+
+def calc_X_Y_Z(raw_address, section_raw_address, section_virtual_address):
+    return raw_address - section_raw_address + section_virtual_address
+
+# Calc the rel_virtual_address
+
+
+def calc_rel_va(old_entry_point, new_entry_point):
+    return old_entry_point - 0x5 - (new_entry_point + 0x14)
+
+# Create payload
+
+
+def create_payload(x, y, z, new_entry_point, rel_virtual_address):
+    payload = ""
+    first_payload = "\x6A\x00"
+    second_payload = "\x68" 
+    return
+
+
+try:
+    # Path
+    path = 'C:\\Users\\ADmin\\Desktop\\TestPlace\\NOTEPAD.exe'
+    pe = pefile.PE(path)
+
+    # Calc the entry_point
+    entry_point_va = calc_entry_point_va(pe)
     print('Entry point va: ', hex(entry_point_va))
+
+    # Get the header section of pe file
+    rsrc_virtual_address, rsrc_raw_address = get_rsrc_section(pe)
+    print("rsrc virtual address: ", rsrc_virtual_address)
+    print("rsrc raw address: ", rsrc_raw_address)
     
-    get_virtual_address(pe)
-
-# except :
-#    pass
-
+    # Calc the x, y, z
+    
 finally:
     print("Done")
