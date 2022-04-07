9b5917adff908b4538660160b3007910712e63fb
ManhKhoa1507 <nguyenlemanhkhoa15072001@gmail.com>
2022-04-07 19:42:40 +0700

    Clean code

--- pe_file.py
+++ pe_file.py
@@ -47,17 +47,17 @@ def create_shell_code(virtual_address_of_caption, virtual_address_of_text, jump_
     shell_code += b'\xFF\x15'
     shell_code += struct.pack("I", address_of_message_box_w)
     shell_code += b'\xE9' + struct.pack("I", jump_address)
-    shell_code += b'\x00' * 23
+    shell_code += b'\x00' * 55
     shell_code += b'\x49\x00\x6e\x00\x66\x00\x6f\x00'
-    shell_code += b'\x00' * 40
+    shell_code += b'\x00' * 24
     shell_code += b'\x49\x00\x6E\x00\x6A\x00\x65\x00\x63\x00\x74\x00\x65\x00\x64\x00\x20\x00\x62\x00\x79\x00\x20\x00\x31\x00\x39\x00\x35\x00\x32\x00\x30\x00\x36\x00\x33\x00\x39\x00\x20\x00\x31\x00\x39\x00\x35\x00\x32\x00\x30\x00\x36\x00\x30\x00\x34\x00\x20\x00\x31\x00\x39\x00\x35\x00\x32\x00\x30\x00\x36\x00\x31\x00\x37'
 
     return shell_code
 
 
 def add_more_space(input, output):
-    shutil.copy2(input, output)
     # Get original_size and add more space to file pe
+    shutil.copy2(input, output)
     print("[*] Resize the Executable")
 
     original_size = os.path.getsize(output)
@@ -74,10 +74,10 @@ def add_more_space(input, output):
 
 def injected_shell_code(input, output):
     # Path to pe file
-    
+
     original_size = add_more_space(input, output)
     pe = pefile.PE(output)
-    raw_address_of_shell_code = original_size 
+    raw_address_of_shell_code = original_size
     print("raw address shellcode: ", hex(raw_address_of_shell_code))
     number_of_sections = get_info_section(pe)
 
@@ -111,7 +111,7 @@ def injected_shell_code(input, output):
     jump_address = ((entry_point_old + image_base) - 5 -
                     (new_entry_point + 0x14)) & 0xffffffff
     print("jump entry: ", hex(jump_address))
-    
+
     # Get the address of message box w
     address_of_message_box_w = get_message_box_w(pe)
 
@@ -121,7 +121,7 @@ def injected_shell_code(input, output):
     # Inject shell code
     print("\nShell-code : ")
     print(shell_code)
-    
+
     print("Inject shell_code at : ", hex(raw_address_of_shell_code))
     print("Inject Caption at: ", hex(raw_address_of_caption))
     print("Inject text at: ", hex(raw_address_of_text))
@@ -136,12 +136,13 @@ def injected_shell_code(input, output):
     pe.write(output)
     print("Inject Successfully!!")
 
+
 file = [
     'NOTEPAD.exe',
     'calc.exe'
 ]
 
-for input_file in file :
+for input_file in file:
     output_file = input_file.replace('.exe', '-injected.exe')
     print("\nInjecting ", input_file)
     print("\n")
